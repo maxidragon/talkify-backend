df68ec4931066de26163e97505085c06f049ffaa
Maksymilian Gala <mgwspinanie2006@wp.pl>
2023-04-27 09:27:30 +0200

    Add loogut

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,20 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Post, Query, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
   @Get('isTaken')
   async isTaken(@Query('email') email: string) {
     return await this.authService.isTaken(email);
+  }
+
+  @Post('logout')
+  async logout(@Res() res: Response) {
+    res.clearCookie('jwt');
+    res.clearCookie('user_info');
+    res.send({ statusCode: true, message: 'Logout success' });
   }
 }
